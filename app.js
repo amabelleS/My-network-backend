f080c95f137a7cf286ce942f966aae4e336ade01
amabelle <amabelletr@gmail.com>
2021-08-13 02:47:51 +0300

    Getting posts

--- app.js
+++ app.js
@@ -2,13 +2,23 @@ const express = require("express");
 
 const app = express();
 
-app.use((req, res, next) => {
-    console.log('first middleware');
-    next();
-})
-
-app.use((req, res, next) => {
-    res.send('Hello from express')
-})
+app.get("/api/posts", (req, res, next) => {
+  const posts = [
+    {
+      id: "fadf12421l",
+      title: "First server-side post",
+      content: "This is coming from the server"
+    },
+    {
+      id: "ksajflaj132",
+      title: "Second server-side post",
+      content: "This is coming from the server!"
+    }
+  ];
+  res.status(200).json({
+    message: "Posts fetched successfully!",
+    posts: posts
+  });
+});
 
 module.exports = app;
